6d778c995c5ef1664ecaa55a01ceda7ed87ee54d
André Silva <andre06x@gmail.com>
2024-02-06 23:30:07 -0300

    add context tables models

---
+++ api-leilao/Repositories/AuctionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using api_leilao.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_leilao.Repositories
+{
+    public class AuctionDbContext: DbContext
+    {
+        public DbSet<Auction> Auctions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Offer> Offers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\andre\\Downloads\\leilaoDbNLW.db");
+        }
+    }
+}
