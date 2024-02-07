daa40cbeaa7e0f08a2d3026d660091e1f0476657
André Silva <andre06x@gmail.com>
2024-02-06 23:29:09 -0300

    add use cases

---
+++ api-leilao/UseCases/Leiloes/GetCurrent/GetCurrentLeiloesUseCase.cs
@@ -0,0 +1,16 @@
+﻿using api_leilao.Entites;
+using api_leilao.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_leilao.UseCases.Leiloes.GetCurrent
+{
+    public class GetCurrentLeiloesUseCase
+    {
+        public Auction Execute()
+        {
+            var repository = new AuctionDbContext();
+
+            return repository.Auctions.Include(auction => auction.Items).First();
+        }
+    }
+}
---
+++ api-leilao/UseCases/Offers/CreateOffer/CreateOfferUseCase.cs
@@ -0,0 +1,31 @@
+﻿using api_leilao.Communication.Requests;
+using api_leilao.Entites;
+using api_leilao.Repositories;
+using api_leilao.Services;
+
+namespace api_leilao.UseCases.Offers.CreateOffer
+{
+    public class CreateOfferUseCase
+    {
+        private readonly LoggedUser _loggedUser;
+
+        public CreateOfferUseCase(LoggedUser loggedUser) => _loggedUser = loggedUser;
+        public int Execute(int itemId, RequestCreateOffer request)
+        {
+            var repository = new AuctionDbContext();
+
+            var user = _loggedUser.User();
+            var offer = new Offer
+            {
+                CreatedOn = DateTime.Now,
+                ItemId = itemId,
+                Price = request.Price,
+                UserId = user.Id,
+            };
+            repository.Offers.Add(offer);
+            repository.SaveChanges();
+
+            return offer.Id;
+        }
+    }
+}
