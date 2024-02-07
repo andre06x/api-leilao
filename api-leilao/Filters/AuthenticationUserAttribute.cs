e3551059d7a0aa5f8995f7c9dbf008e2563a8fb2
André Silva <andre06x@gmail.com>
2024-02-06 23:31:06 -0300

    add auth filter

---
+++ api-leilao/Filters/AuthenticationUserAttribute.cs
@@ -0,0 +1,47 @@
+﻿using api_leilao.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace api_leilao.Filters
+{
+    public class AuthenticationUserAttribute: AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+
+                var token = TokenOnRequest(context.HttpContext);
+
+                var repository = new AuctionDbContext();
+                var email = FromBase64String(token);
+
+                var exist = repository.Users.Any(user => user.Email.Equals(email));
+                if (exist == false)
+                {
+                    context.Result = new UnauthorizedObjectResult("Email not valid");
+                }
+            
+
+        }
+
+        private string TokenOnRequest(HttpContext context)
+        {
+            var authentication = context.Request.Headers.Authorization.ToString();
+            if (string.IsNullOrEmpty(authentication))
+            {
+                throw new Exception("Token is missing.");
+            }
+            return authentication["Bearer ".Length..];
+
+        }
+
+        public string FromBase64String(string base64)
+        {
+            var data = Convert.FromBase64String(base64);
+            return System.Text.Encoding.UTF8.GetString(data);
+        }
+    }
+
+
+}
