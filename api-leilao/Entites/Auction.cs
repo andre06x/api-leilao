da91479cb893397c2df789a65679f601a54d895e
André Silva <andre06x@gmail.com>
2024-02-06 23:30:46 -0300

    add entites

---
+++ api-leilao/Entites/Auction.cs
@@ -0,0 +1,13 @@
+﻿namespace api_leilao.Entites
+{
+    public class Auction
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;  
+        public DateTime Starts { get; set; }    
+        public DateTime Ends { get; set; }
+
+        public List<Item> Items { get; set; } = new List<Item>();
+
+    }
+}
---
+++ api-leilao/Entites/Item.cs
@@ -0,0 +1,18 @@
+﻿using api_leilao.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_leilao.Entites
+{
+    [Table("Items")]
+    public class Item
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }  = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public Condition Condition {  get; set; } 
+
+        public decimal BasePrice { get; set; }  
+        public int AuctionId { get; set; }
+
+    }
+}
