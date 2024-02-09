904bdbcfbb40a3e00b881ef8c5722b6f10714583
André Silva <andre06x@gmail.com>
2024-02-08 22:31:30 -0300

    refatorando logged user repository

--- api-leilao/Services/LoggedUser.cs
+++ api-leilao/Services/LoggedUser.cs
@@ -1,4 +1,5 @@
-﻿using api_leilao.Entites;
+﻿using api_leilao.Contracts;
+using api_leilao.Entites;
 using api_leilao.Repositories;
 
 namespace api_leilao.Services
@@ -6,15 +7,16 @@ namespace api_leilao.Services
     public class LoggedUser
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
-        public LoggedUser(IHttpContextAccessor httpContext) {
+        private readonly IUserRepository _repository;
+        public LoggedUser(IHttpContextAccessor httpContext, IUserRepository repository) {
             _httpContextAccessor = httpContext;
+            _repository = repository;
         }
         public User User()
         {
-            var repository = new AuctionDbContext();
             var token = TokenOnRequest();
             var email = FromBase64String(token);
-            return repository.Users.First(user => user.Email.Equals(email));
+            return _repository.GetUserByEmail(email);
         }
 
         private string TokenOnRequest()
